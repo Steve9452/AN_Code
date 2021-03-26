7720962868c2d39a54300e84418398ef357a4b67
Steve9452 <00380119@uca.edu.sv>
2021-03-25 22:47:05 -0600

    Bisection2 fn fixed

--- AN_Biseccion/Program.cs
+++ AN_Biseccion/Program.cs
@@ -8,13 +8,16 @@ namespace AN_Biseccion
         {
             Console.WriteLine("Metodo de biseccion: ");
             Func<double, double> f1 = x => Math.Cos(x) - x;
-            Func<double, double> f2 = x => -0.4 + 1.74 * Math.Log(  Math.Sqrt(x)) - Math.Sqrt(1/x);
+            Func<double, double> f2 = x => -0.4 + 1.74 * Math.Log( Math.Pow(10,4) * Math.Sqrt(x)) - Math.Sqrt(1/x); 
             Func<double,double,double> pow = (n1 , n2) => Math.Pow(n1, n2);
 
             //Bisection(f1, 0, 1, pow(10,-6), 100000);
+            //Bisection2(f1,0,1,pow(10,-6));
+            Bisection2(f2, 0.005, 0.01, pow(10,-20));
             Bisection2(f1,0,1,pow(10,-6));
+            //Bisection(f2, 0.005, 0.01, Math.Pow(10,-20),);
         }
-
+        //Necesita un valor de limite de iteraciones especifico
         public static void Bisection(Func<double , double> f, double a, double b, double TOL, int Nmax)
                 {
                     double p = 0;
@@ -30,18 +33,18 @@ namespace AN_Biseccion
                             a = p;
                     }
                 }
+        //No requiere un Nmax especifico
         public static void Bisection2(Func<double , double> f, double a, double b, double TOL)
         {   
 
             double p = 0;
-            double Nmax = Math.Ceiling( Math.Log((b - a) / TOL, 2));
+            // Math.Ceiling aproxima al valor inmediatamente superior de un decimal. si es un entero exacto deja ese
+            double Nmax = Math.Ceiling( Math.Log((b - a) / TOL, 2));             
             for (int i = 0 ; i < Nmax ; i++) 
             {
-                p = (a + b) /2;
+                p = (a + b) / 2;
                 Console.WriteLine($"{i}\t{p}\t{f(p)}");
-                if ( f(p) == 0 || (b - a) / 2 < TOL)
-                    break;
-                else if (f(a) * f(p) < 0)
+                if (f(a) * f(p) < 0)
                     b = p;
                 else
                     a = p;
